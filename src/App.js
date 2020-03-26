9229369d704f80fd409bb4b943bc55701cbaf0c6
Diyar Kudrat <dkudrat@gmail.com>
2020-03-26 14:53:03 -0700

    Type in any query in search bar, will return objects related to query

--- src/App.js
+++ src/App.js
@@ -10,24 +10,40 @@ const App = () => {
   const APP_KEY = 'b25709aba8348170439ee4ef9cd78d1b';
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits)
+    // console.log(data.hits)
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    // console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
   }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input 
+          className="search-bar" 
+          type="text" value={search} 
+          onChange={updateSearch} 
+        />
         <button className="search-btn" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
